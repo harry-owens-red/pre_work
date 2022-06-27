274295de7662f4e28e613c5063e2a34a3ef895ec
Harry Owens <harry.owens@sky.uk>
2022-06-27 13:09:13 +0100

    fixed utility functions

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -45,6 +45,13 @@ int sumNumbersFromGenerators(std::vector<GeneratorType> const & types, int num)
     // and creates generator for given type (use createGenerator function),
     // then sums up all generated values for each type and returns the sum of sums.
     // Use sumGeneratedNumbers to sum generated values for each generator.
-    return 42;
+    int sum = 0;
+
+    for(GeneratorType genType : types)
+    {
+        sum += sumGeneratedNumbers(createGenerator(genType), num);
+    }
+
+    return sum;
 }
 
